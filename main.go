b51c1187f41358457a7bd6b80b5e7a3c93207cd0
disappearcompletely <d.komarov2019@gmail.com>
2025-05-20 00:56:40 +0300

    work

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func Add(a, b int) int {
+	return a + b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
 func main() {
-	fmt.Printf("3 + 4 = %d\n", Add(3, 4))
+	sum := Add(3, 4)
+	product := Multiply(3, 4)
+	fmt.Printf("3 + 4 = %d\n", sum)
+	fmt.Printf("3 * 4 = %d\n", product)
 }
--- main_test.go
+++ main_test.go
@@ -5,8 +5,15 @@ import "testing"
 func TestAdd(t *testing.T) {
 	got := Add(2, 5)
 	want := 7
-
 	if got != want {
 		t.Errorf("Add(2, 5) = %d; want %d", got, want)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	got := Multiply(3, 4)
+	want := 12
+	if got != want {
+		t.Errorf("Multiply(3, 4) = %d; want %d", got, want)
 	}
 }
